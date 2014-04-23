668d1ae0efa0e0782600860add48de5fe9faef15
Denis Usanov <usanovdd@gmail.com>
2014-04-24 01:10:53 +0400

    Skeleton for nutrients

--- pynutrients/nutrients.py
+++ pynutrients/nutrients.py
@@ -1,2 +1,13 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+
+class Nutrients(object):
+
+    def __init__(self, provider):
+        self.provider = provider
+
+    def set_provider(self, provider):
+        self.provider = provider
+
+    def request(self, name):
+        pass
