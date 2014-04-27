ff1962a7483abee50f9ab5b776de290a67c495c9
Denis Usanov <usanovdd@gmail.com>
2014-04-28 01:21:30 +0400

    Healthdiet mock

---
+++ pynutrients/providers/healthdiet.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from providers.provider import Provider
+
+
+class HealthDiet(Provider):
+
+    def __init__(self):
+        pass
+
+    def request_product(self, name):
+        pass
