4216be1ab72b8cd4996b3a61f0e96e8da466d6d2
Denis Usanov <usanovdd@gmail.com>
2014-05-07 02:20:40 +0400

    Setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Pynutrients',
+      version='0.1',
+      description='Python package to get food nutrients',
+      author='Denis Usanov',
+      author_email='denis@usanov.info',
+      url='https://github.com/dck/pynutrients',
+      packages=['pynutrients', 'pynutrients.providers'],
+     )
